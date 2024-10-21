94c5f38a0403ddc8c1757db8b8f07c2f9bf4d393
segmue <sebigmuer@gmail.com>
2024-10-21 09:35:59 +0200

    added code to create lightweight gazeteer

--- preprocess_swissnames_to_gazeteer.py
+++ preprocess_swissnames_to_gazeteer.py
@@ -1,7 +1,30 @@
+import io
+import zipfile
+
 import pandas as pd
-swissnames = pd.read_csv('data/swissNAMES3D_PKT.csv', sep=';', usecols=['OBJEKTART', 'NAME'])
+import requests
+swissnames = pd.read_csv('data/swissNAMES3D_PKT.csv', sep=';', usecols=['OBJEKTART','OBJEKTKLASSE_TLM', 'NAME','E','N'])
 
-# Filter out only the relevant columns OBJEKTART and NAME
 
-## Make one dictionary with Name as key and OBJEKTART as value
-swissnames_dict = dict(zip(swissnames['NAME'], swissnames['OBJEKTART']))
+# Download Zip from link and unzip it in memory: https://data.geo.admin.ch/ch.swisstopo.swissnames3d/swissnames3d_2022/swissnames3d_2022_2056.csv.zip
+url = 'https://data.geo.admin.ch/ch.swisstopo.swissnames3d/swissnames3d_2022/swissnames3d_2022_2056.csv.zip'
+r = requests.get(url)
+z = zipfile.ZipFile(io.BytesIO(r.content))
+z.extractall()
+res = pd.DataFrame()
+for name in z.namelist():
+    if name.endswith('.csv'):
+        df = pd.read_csv(z.open(name), sep=';',usecols=['OBJEKTART','OBJEKTKLASSE_TLM', 'NAME','E','N'])
+        res = pd.concat([res, df])
+
+# REMOVE DUPLICATES
+res = res.drop_duplicates()
+
+
+## Make a index dictionary. Key is "Name" and Value is Number of Occurences
+swissnames_dict = dict((res['NAME']).str.lower().value_counts())
+
+## Save the dictionary to a csv
+swissnames_dict = pd.DataFrame.from_dict(swissnames_dict, orient='index').reset_index()
+swissnames_dict.columns = ['Name', 'Count']
+swissnames_dict.to_csv('swissnames_dict.csv', index=False)

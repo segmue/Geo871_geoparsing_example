71f86752e459fc7b30a15e5236b0c27179e8b2f6
Sebastian Gmür <89509043+segmue@users.noreply.github.com>
2024-10-21 15:27:32 +0200

    Update preprocess_swissnames_to_gazeteer.py
    cleaned up preprocess

--- preprocess_swissnames_to_gazeteer.py
+++ preprocess_swissnames_to_gazeteer.py
@@ -1,10 +1,7 @@
 import io
 import zipfile
-
 import pandas as pd
 import requests
-swissnames = pd.read_csv('data/swissNAMES3D_PKT.csv', sep=';', usecols=['OBJEKTART','OBJEKTKLASSE_TLM', 'NAME','E','N'])
-
 
 # Download Zip from link and unzip it in memory: https://data.geo.admin.ch/ch.swisstopo.swissnames3d/swissnames3d_2022/swissnames3d_2022_2056.csv.zip
 url = 'https://data.geo.admin.ch/ch.swisstopo.swissnames3d/swissnames3d_2022/swissnames3d_2022_2056.csv.zip'
@@ -17,11 +14,8 @@ for name in z.namelist():
         df = pd.read_csv(z.open(name), sep=';',usecols=['OBJEKTART','OBJEKTKLASSE_TLM', 'NAME','E','N'])
         res = pd.concat([res, df])
 
-# REMOVE DUPLICATES
 res = res.drop_duplicates()
 
-
-## Make a index dictionary. Key is "Name" and Value is Number of Occurences
 swissnames_dict = dict((res['NAME']).str.lower().value_counts())
 
 ## Save the dictionary to a csv
